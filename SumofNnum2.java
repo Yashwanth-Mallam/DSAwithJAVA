8f327241b651cd1eb6754f4f8eaa971d331a9d29
Yashwanth <yashwanthmallam345@gmail.com>
2025-09-15 17:39:39 +0530

    basic Recurtion problems using java

--- SumofNnum2.java
+++ SumofNnum2.java
@@ -18,6 +18,15 @@ public class SumofNnum2 {
         s.close();
     }
 
+    // same using recurtion
+    public static int sumOfNaturalNumbers(int n){
+        if(n==0){
+            return 0;
+        }
+        return n + sumOfNaturalNumbers(n - 1);
+
+    }
+
     // main method
 
     public static void main(String[] args) {
